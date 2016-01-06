da31f40ee8eedf97d32cbf66744c79c5a350c0dd
Stanislas Nanchen <stan@deep-impact.ch>
2016-01-06 10:05:28 +0100

    Posting plugin for antigen.

---
+++ plugins/posting/posting.plugin.zsh
@@ -0,0 +1,14 @@
+# mongo db
+
+export PATH="${PATH}:/Users/stan/projects/posting/mongo/mongodb-osx-x86_64-2.4.14/bin"
+
+# cassandra
+
+export PATH="${PATH}:/Users/stan/projects/posting/cassandra/apache-cassandra-1.2.19/bin"
+
+# elastic search
+
+export PATH="${PATH}:/Users/stan/projects/posting/elasticsearch/elasticsearch-1.4.5/bin"
+
+# mysql
+
